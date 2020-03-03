d6fcc65ffea156db807cd0aba4292e97fac3f828
Steph Torres <58521245+netorrespanez@users.noreply.github.com>
2020-03-03 17:30:10 +0100

    Exercises on the manipulation of the DOM
    Class 13

--- recursos/Clase 13/Product Component/index.js
+++ recursos/Clase 13/Product Component/index.js
@@ -1,11 +1,11 @@
 var  variants =  [
     {
       variantColor: 'green',
-      variantImage:  'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
+      variantImage:  'https://www.futbolemotion.com/imagesarticulos/132072/750/gorra-adidas-real-madrid-c40-2019-2020-hi-re-green-night-indigo-white-0.jpg'
     },
     {
       variantColor: 'blue',
-      variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',    
+      variantImage: 'https://www.futbolemotion.com/imagesarticulos/132071/750/gorra-adidas-real-madrid-c40-2019-2020-night-indigo-black-matte-gold-0.jpg',    
     }
   ]
 
