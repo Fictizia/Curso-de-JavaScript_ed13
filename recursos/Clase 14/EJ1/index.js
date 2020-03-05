2b07eb81cf659e94477d1225c9888febed8c6b0f
Steph Torres <58521245+netorrespanez@users.noreply.github.com>
2020-03-05 17:14:26 +0100

    Exercises on the manipulation of the DOM
    Exercises on the manipulation of the DOM.
    New functionalities to manipulate DOM nodes.

---
+++ recursos/Clase 14/EJ1/index.js
@@ -0,0 +1,20 @@
+var btn = document.getElementsByTagName('button');
+var popupDiv = document.getElementsByClassName('popup_main_div');
+//var closePopup = document.getElementsByClassName("close_popup");
+
+span.replaceChild(newChild, oldChild);
+
+  function openPopUp() {
+    popupDiv[0].style.display="block"; 
+  }
+  /* Btn.addEventListener('click',function(){
+    PopupDiv.style.display="block"
+  }) */
+
+  function closePopUp() {
+    popupDiv[0].style.display="none";
+  }
+
+  /* ClosePopup.addEventListener('click',function(){
+    PopupDiv.style.display="none"
+  }) */
