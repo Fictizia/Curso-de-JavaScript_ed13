0317595bd6e894461d6c691d87bfefa30b67ec69
Steph Torres <58521245+netorrespanez@users.noreply.github.com>
2020-03-05 14:22:19 +0100

    Exercise with features added (replaceChild)
    The same exercise is performed initially with added functionality

--- recursos/Clase 13/Product Component/index.js
+++ recursos/Clase 13/Product Component/index.js
@@ -1,18 +1,28 @@
-var  variants =  [
-    {
-      variantColor: 'green',
-      variantImage:  'https://www.futbolemotion.com/imagesarticulos/132072/750/gorra-adidas-real-madrid-c40-2019-2020-hi-re-green-night-indigo-white-0.jpg'
-    },
-    {
-      variantColor: 'blue',
-      variantImage: 'https://www.futbolemotion.com/imagesarticulos/132071/750/gorra-adidas-real-madrid-c40-2019-2020-night-indigo-black-matte-gold-0.jpg',    
+var items = [
+    {ref: 'ADDY7721', img: 'https://www.futbolemotion.com/imagesarticulos/132071/750/gorra-adidas-real-madrid-c40-2019-2020-night-indigo-black-matte-gold-0.jpg'},
+    {ref: 'ADDY7722',img: 'https://www.futbolemotion.com/imagesarticulos/132072/750/gorra-adidas-real-madrid-c40-2019-2020-hi-re-green-night-indigo-white-0.jpg'},
+    {ref: 'ADDY7720',img: 'https://www.futbolemotion.com/imagesarticulos/132070/750/gorra-adidas-real-madrid-c40-2019-2020-white-dark-football-gold-0.jpg'}];
+
+var totalItems = items.length;
+var initialPosition = 0;
+
+function changeItem () { //0
+    initialPosition++;   //1
+
+    if (initialPosition === totalItems) {
+        initialPosition = 0;
     }
-  ]
 
-  function updateProduct(index) {  
-    let imgNode = document.querySelector('img');
-    imgNode.src = variants[index].variantImage;
+    let productImg = document.getElementsByTagName('img');
+    productImg[0].src = items[initialPosition].img;
 
-    //selectedElement.style.backgroundColor = variants[index].variantColor;
-    //style="{ backgroundColor: variant.variantColor }"
+    let div = document.getElementsByClassName('container');
+    let description = document.getElementsByTagName('p');
+    
+    let label = items[initialPosition].ref;
+    let refText = document.createTextNode(label);
+    let newDescription = document.createElement('p');
+
+    newDescription.appendChild(refText);
+    div[0].replaceChild(newDescription, description[0]);
 }
